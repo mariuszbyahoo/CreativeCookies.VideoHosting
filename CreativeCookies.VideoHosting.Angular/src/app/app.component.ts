8f620ccf4253df3ae1c4af4632aa5636f3df9159
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-03-20 20:53:26 +0100

    adding plyr POC with angular app

--- CreativeCookies.VideoHosting.Angular/src/app/app.component.ts
+++ CreativeCookies.VideoHosting.Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CreativeCookies.VideoHosting.Angular';
+  title = 'MyTube';
+
+  constructor(private modalService: NgbModal){
+
+  }
+
+  open(modal: any){
+    this.modalService.open(modal);
+  }
 }
