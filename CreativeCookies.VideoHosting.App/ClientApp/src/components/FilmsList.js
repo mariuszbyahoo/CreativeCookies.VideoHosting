3878acc02f4e1db7f32be50bb9e7ecc503f93e5b
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-04-08 10:53:28 +0200

    Setting filter by blob name

--- CreativeCookies.VideoHosting.App/ClientApp/src/components/FilmsList.js
+++ CreativeCookies.VideoHosting.App/ClientApp/src/components/FilmsList.js
@@ -16,6 +16,7 @@ import { Search } from "@mui/icons-material";
 
 const FilmsList = () => {
   const [filmBlobs, setFilmBlobs] = useState([]);
+  const [filteredFilmBlobs, setFilteredFilmBlobs] = useState([]);
   const [thumbnailBlobs, setThumbnailBlobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
@@ -27,7 +28,9 @@ const FilmsList = () => {
       .then((token) => {
         listBlobs(process.env.REACT_APP_CONTAINER_NAME, token)
           .then((blobs) => {
-            setFilmBlobs(blobs.filter((b) => b.name.includes(".mp4")));
+            const filmBlobs = blobs.filter((b) => b.name.includes(".mp4"));
+            setFilmBlobs(filmBlobs);
+            setFilteredFilmBlobs(filmBlobs); // Set the filteredFilmBlobs here
             setThumbnailBlobs(
               blobs.filter((b) => b.name.includes(".png")).map((b) => b.name)
             );
@@ -49,9 +52,6 @@ const FilmsList = () => {
   }, [fetchMoviesHandler]); // Empty array to run the effect only once, when the component mounts.
 
   async function fetchSasToken() {
-    console.log(
-      `process.env.REACT_APP_API_ADDRESS: ${process.env.REACT_APP_API_ADDRESS}`
-    );
     const response = await fetch(
       `https://${process.env.REACT_APP_API_ADDRESS}/api/SAS/container/`
     );
@@ -76,11 +76,11 @@ const FilmsList = () => {
   }
 
   const filterInputChangeHandler = (e) => {
-    console.log(e.target.value);
-  };
-
-  const filterByChangeHandler = (e) => {
-    console.log(e.target.value);
+    setFilteredFilmBlobs(
+      filmBlobs.filter((b) =>
+        b.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    ); // Update the filteredFilmBlobs instead
   };
 
   let content = <p>Upload a movie to get started!</p>;
@@ -91,54 +91,35 @@ const FilmsList = () => {
     content = <h4>An error occured, while fetching the API: {error}</h4>;
   }
 
-  if (filmBlobs.length > 0) {
+  if (filteredFilmBlobs.length > 0) {
     // Order by date desc
-    filmBlobs.sort(
+    filteredFilmBlobs.sort(
       (a, b) =>
         new Date(b.properties.createdOn) - new Date(a.properties.createdOn)
     );
-    content = <Mosaic filmBlobs={filmBlobs} thumbnailBlobs={thumbnailBlobs} />;
+    content = (
+      <Mosaic filmBlobs={filteredFilmBlobs} thumbnailBlobs={thumbnailBlobs} />
+    );
   }
 
   return (
     <div className={styles.container}>
       <div className="row">
-        <div className="col-6">
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <TextField
-              label="Filter"
-              id="filter-search"
-              onChange={filterInputChangeHandler}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                ),
-              }}
-              variant="filled"
-            />
-          </FormControl>
-        </div>
-        <div className="col-6">
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={20}
-              onChange={filterByChangeHandler}
-              label="Age"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <TextField
+            label="Filter"
+            id="filter-search"
+            onChange={filterInputChangeHandler}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            variant="filled"
+          />
+        </FormControl>
       </div>
       {content}
     </div>
