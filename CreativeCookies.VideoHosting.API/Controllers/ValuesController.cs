47eca714aa216c887ed3c33255504c4339b0ecc5
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-04-16 14:23:54 +0200

    adding testing ValuesController

---
+++ CreativeCookies.VideoHosting.API/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreativeCookies.VideoHosting.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ActionResult<string> GetIndex()
+        {
+            return Ok("And my new Release pipeline works!!! - This was non existent prior CI&CD");
+        }
+    }
+}
