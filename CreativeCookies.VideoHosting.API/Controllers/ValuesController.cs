aeb8da2e2b88835d0e4f9adbd115c52c5d48ece3
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-04-16 14:47:13 +0200

    adding values controller

---
+++ CreativeCookies.VideoHosting.API/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreativeCookies.VideoHosting.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetIndex()
+        {
+            return Ok("And my new Release pipeline works!!! - This was non existent prior CI&CD");
+        }
+    }
+}
