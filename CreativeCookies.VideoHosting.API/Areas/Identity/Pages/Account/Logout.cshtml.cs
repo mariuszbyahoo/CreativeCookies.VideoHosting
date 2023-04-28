bf03748d8167b21627435d7d571311fcfa2cec2a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-04-28 17:56:23 +0200

    adding OnGetAsync to Logout view model

--- CreativeCookies.VideoHosting.API/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ CreativeCookies.VideoHosting.API/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -17,10 +17,27 @@ namespace CreativeCookies.VideoHosting.API.Areas.Identity.Pages.Account
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
+
         public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                // This needs to be a redirect so that the browser performs a new
+                // request and the identity for the user gets updated.
+                return RedirectToPage();
+            }
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
