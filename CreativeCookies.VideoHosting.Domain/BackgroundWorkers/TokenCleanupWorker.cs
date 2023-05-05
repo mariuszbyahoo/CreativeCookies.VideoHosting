bd8359d63e69c2da16a322421d88926d8d9a52a4
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-05-05 12:04:04 +0200

    making worker perform work once for six hours and adding new profile

--- CreativeCookies.VideoHosting.Domain/BackgroundWorkers/TokenCleanupWorker.cs
+++ CreativeCookies.VideoHosting.Domain/BackgroundWorkers/TokenCleanupWorker.cs
@@ -31,7 +31,7 @@ namespace CreativeCookies.VideoHosting.Domain.BackgroundWorkers
                         // HACK: TODO Add expired tokens cleanup!
                         await authCodeRepository.ClearExpiredAuthorizationCodes();
                     }
-                    await Task.Delay(TimeSpan.FromMinutes(15), stoppingToken);
+                    await Task.Delay(TimeSpan.FromHours(6), stoppingToken);
                 }
             }
         }
