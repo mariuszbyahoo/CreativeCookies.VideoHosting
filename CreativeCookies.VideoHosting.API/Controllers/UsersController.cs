ebaacd3bc5e0dabf9a0ffe87ef8b2e4397e300fa
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-06-26 14:18:34 +0200

    adding UsersController for admin

---
+++ CreativeCookies.VideoHosting.API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using CreativeCookies.VideoHosting.Contracts.DTOs.OAuth;
+using CreativeCookies.VideoHosting.Domain.DTOs.OAuth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CreativeCookies.VideoHosting.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "admin,ADMIN")]
+        public async Task<ActionResult<IList<IMyHubUser>>> GetAll()
+        {
+            var users = _userManager.Users.ToList();
+            var result = new List<IMyHubUser>();
+            foreach (var user in users)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                result.Add(new MyHubUserDto(Guid.Parse(user.Id.ToUpperInvariant()), user.Email ?? user.UserName, string.Join(',',userRoles)));
+            }
+            return result;
+        }
+    }
+}
