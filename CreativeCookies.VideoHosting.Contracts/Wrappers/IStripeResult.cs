8f9dac01fe26c2ca88598291151624c0dacd5fb1
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-08-11 20:03:06 +0200

    Adding a wrapper for ambigous Stripe's response

---
+++ CreativeCookies.VideoHosting.Contracts/Wrappers/IStripeResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreativeCookies.VideoHosting.Contracts.Wrappers
+{
+    public interface IStripeResult<T>
+    {
+        bool Success { get; set; }
+        T Data { get; set; }
+        string ErrorMessage { get; set; }
+    }
+
+}
---
+++ CreativeCookies.VideoHosting.Domain/Wrappers/StripeResult.cs
@@ -0,0 +1,17 @@
+﻿using CreativeCookies.VideoHosting.Contracts.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreativeCookies.VideoHosting.Domain.Wrappers
+{
+    public class StripeResult<T> : IStripeResult<T>
+    {
+        public bool Success { get; set; }
+        public T Data { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+}
