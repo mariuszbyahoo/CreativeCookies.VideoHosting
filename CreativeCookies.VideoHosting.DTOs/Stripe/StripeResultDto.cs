cd026f87d7f9f71f3cc70bf189accdedd8252363
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-08-14 17:55:05 +0200

    Changing StripeResultDto.Data into nullable property

--- CreativeCookies.VideoHosting.DTOs/Stripe/StripeResultDto.cs
+++ CreativeCookies.VideoHosting.DTOs/Stripe/StripeResultDto.cs
@@ -9,10 +9,10 @@ namespace CreativeCookies.VideoHosting.DTOs.Stripe
     public class StripeResultDto<T>
     {
         public bool Success { get; set; }
-        public T Data { get; set; }
+        public T? Data { get; set; }
         public string ErrorMessage { get; set; }
 
-        public StripeResultDto(bool success, T data, string errorMessage)
+        public StripeResultDto(bool success, T? data, string errorMessage)
         {
             Success = success;
             Data = data;
