de71f0e10f29e9296e7d99a2bfdb4a258943da71
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-08-15 18:27:30 +0200

    Adding IMyHubUserManager wrapper interface

---
+++ CreativeCookies.VideoHosting.Contracts/Services/IdP/IMyHubUserManager.cs
@@ -0,0 +1,99 @@
+﻿
+using CreativeCookies.VideoHosting.DTOs.OAuth;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+
+namespace CreativeCookies.VideoHosting.Contracts.Services.IdP
+{
+    /// <summary>
+    /// Interface serves as a wrapper around UserManager
+    /// Hides the DAL references from API.
+    /// </summary>
+    public interface IMyHubUserManager
+    {
+        Task<IdentityResult> CreateAsync(MyHubUserDto user, string password);
+
+        Task<IdentityResult> AddToRoleAsync(MyHubUserDto user, string role);
+
+        Task<IList<string>> GetRolesAsync();
+
+        string? GetUserId(ClaimsPrincipal principal);
+
+        Task<MyHubUserDto?> FindByIdAsync(Guid id);
+
+        Task<MyHubUserDto> GetUserAsync(ClaimsPrincipal principal);
+
+        Task<IdentityResult> SetUserNameAsync(MyHubUserDto user, string userName);
+
+        #region email
+
+        Task<MyHubUserDto?> FindByEmailAsync(string email);
+
+        /// <summary>
+        /// Getter for UserManager's property
+        /// </summary>
+        /// <returns>SupportsUserEmail propperty</returns>
+        bool GetManagerSupportsuserEmail();
+
+        Task<string> GenerateChangeEmailTokenAsync(MyHubUserDto user, string newEmail);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(MyHubUserDto user);
+
+        Task<string?> GetEmailAsync(MyHubUserDto user);
+
+        Task<IdentityResult> ConfirmEmailAsync(MyHubUserDto user, string token);
+
+        Task<IdentityResult> ChangeEmailAsync(MyHubUserDto user, string newEmail, string token);
+
+        Task<bool> IsEmailConfirmedAsync(MyHubUserDto user);
+
+        #endregion
+
+        #region password
+
+        Task<string> GeneratePasswordResetTokenAsync(MyHubUserDto user);
+
+        Task<bool> HasPasswordAsync(MyHubUserDto user);
+
+        Task<MyHubUserDto> ChangePasswordAsync(MyHubUserDto user, string currentPassword, string newPassword);
+
+        Task<bool> CheckPasswordAsync(MyHubUserDto user, string password);
+
+        #endregion
+
+        #region twoFactor
+
+        Task<int> CountRecoveryCodesAsync(MyHubUserDto user);
+
+        Task<IdentityResult> ResetAuthenticatorKeyAsync(MyHubUserDto user);
+
+        /// <summary>
+        /// Below method encapsulates an UserManager's field
+        /// </summary>
+        /// <returns>UserManager.Options.Tokens.AuthenticatorTokenProvider value</returns>
+        Task<string> GetAuthenticatorTokenProvider();
+
+        Task<string?> GetAuthenticatorKeyAsync(MyHubUserDto user);
+
+        Task<bool> VerifyTwoFactorTokenAsync(MyHubUserDto user, string tokenProvider, string token);
+
+        Task<bool> GetTwoFactorEnabledAsync(MyHubUserDto user);
+
+        Task<IEnumerable<string>?> GenerateNewTwoFactorRecoveryCodesAsync(MyHubUserDto user, int number);
+
+        Task<IdentityResult> SetTwoFactorEnabledAsync(MyHubUserDto user, bool enabled);
+
+        #endregion
+
+        Task<IdentityResult> DeleteAsync(MyHubUserDto user);
+
+        Task<IdentityResult> RemoveLoginAsync(MyHubUserDto user, string loginProvider, string providerKey);
+
+        Task<IList<UserLoginInfo>> GetLoginsAsync(MyHubUserDto user);
+
+        Task<string?> GetPhoneNumberAsync(MyHubUserDto user);
+
+        Task<IdentityResult> SetPhoneNumberAsync(MyHubUserDto user, string phoneNumber);
+
+    }
+}
