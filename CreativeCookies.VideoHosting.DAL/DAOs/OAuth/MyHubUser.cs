6ceeac5af44110ac6345e5f363141ecf2b6c5058
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-08-15 18:27:59 +0200

    Adding MyHubUser do DAL

---
+++ CreativeCookies.VideoHosting.DAL/DAOs/OAuth/MyHubUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CreativeCookies.VideoHosting.DAL.DAOs.OAuth
+{
+    public class MyHubUser : IdentityUser<Guid>
+    {
+        public string StripeCustomerId { get; set; }
+    }
+}
