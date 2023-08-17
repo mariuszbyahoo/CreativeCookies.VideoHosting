9433232f2860ece48d6fdc7ef16fb5d75cae65b3
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-08-17 13:54:14 +0200

    -||-

--- CreativeCookies.VideoHosting.DTOs/OAuth/MyHubUserDto.cs
+++ CreativeCookies.VideoHosting.DTOs/OAuth/MyHubUserDto.cs
@@ -7,13 +7,15 @@ namespace CreativeCookies.VideoHosting.DTOs.OAuth
         public string UserEmail { get; set; }
         public string Role { get; set; }
         public bool IsActive { get; set; }
+        public string StripeCustomerId { get; set; }
 
-        public MyHubUserDto(Guid id, string userEmail, string role, bool isActive)
+        public MyHubUserDto(Guid id, string userEmail, string role, bool isActive, string stripeCustomerId)
         {
             Id = id;
             UserEmail = userEmail;
             Role = role;
             IsActive = isActive;
+            StripeCustomerId = stripeCustomerId;
         }
     }
 }
