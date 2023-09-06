9979d02811e637ec36fc866ac0660e7a2ccfc243
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-09-06 20:17:38 +0200

    Adding ITargetUrlService with implementation's stub

---
+++ CreativeCookies.StripeEvents.Contracts/ITargetUrlService.cs
@@ -0,0 +1,12 @@
+﻿namespace CreativeCookies.StripeEvents.Contracts
+{
+    public interface ITargetUrlService
+    {
+        /// <summary>
+        /// Method queries underlying DAL for a record with matching adminEmail, and returns it's targetUrl value.
+        /// </summary>
+        /// <param name="adminEmail">Key to look up for</param>
+        /// <returns>Target URL of that record</returns>
+        public string GetDestinationUrl(string adminEmail);
+    }
+}
---
+++ CreativeCookies.StripeEvents.Services/TargetUrlService.cs
@@ -0,0 +1,14 @@
+﻿using CreativeCookies.StripeEvents.Contracts;
+
+namespace CreativeCookies.StripeEvents.Services
+{
+    public class TargetUrlService : ITargetUrlService
+    {
+        public string GetDestinationUrl(string adminEmail)
+        {
+            // 1. Get record from underlying DAL with matching adminEmail
+            // 2. Return it's destinationUrl value.
+            throw new NotImplementedException();
+        }
+    }
+}
