78d9f5f37405750e4aeed3f42aba801c986edddf
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-09-07 13:50:18 +0200

    adding DeployedInstancesEntity and implementing TargetUrlService

--- CreativeCookies.StripeEvents.RedistributionService/Services/TargetUrlService.cs
+++ CreativeCookies.StripeEvents.RedistributionService/Services/TargetUrlService.cs
@@ -1,14 +1,25 @@
-﻿using CreativeCookies.StripeEvents.RedistributionService.Contracts;
+﻿using Azure.Data.Tables;
+using CreativeCookies.StripeEvents.RedistributionService.Contracts;
+using CreativeCookies.StripeEvents.RedistributionService.DAO;
+using System.Xml;
 
 namespace CreativeCookies.StripeEvents.RedistributionService.Services
 {
     public class TargetUrlService : ITargetUrlService
     {
-        public string GetDestinationUrl(string adminUrl)
+        public string GetDestinationUrl(string adminEmail)
         {
-            // 1. Get record from underlying DAL with matching adminEmail
-            // 2. Return it's destinationUrl value.
-            throw new NotImplementedException();
+            // Initialize your TableClient
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=cccentralstorageaccount;AccountKey=CH1ZTJ0J4qBLpURUOMM0wIrRZX99HOBKeSdmYvCeKn45BXtdmjDGQKSU0ZYzjoFZtVkB5ZBoOdnT+AStIOuVTQ==;EndpointSuffix=core.windows.net";
+            var tableClient = new TableClient(new Uri($"https://cccentralstorageaccount.table.core.windows.net/"), "DeployedInstances", new TableSharedKeyCredential("<DeployedInstances>", storageConnectionString));
+
+            // HACK TODO: Get StorageConnectionString from App's settings - Azure Web Application's Configuration Key Value Pairs.
+
+            // Construct the filter query
+            string filter = TableClient.CreateQueryFilter<DeployedInstancesEntity>(e => e.RowKey.Equals(adminEmail));
+            var entity = tableClient.Query<DeployedInstancesEntity>(filter).FirstOrDefault();
+            if (entity == null) return string.Empty;
+            else return entity.PartitionKey;
         }
     }
 }
