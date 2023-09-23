c138098799fe72a6bd0e57dd0181c4dc46692913
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-09-23 22:57:22 +0200

    adding subscription deletion with full refunds (Stripe fees are being paid from the merchant's account)

--- CreativeCookies.VideoHosting.API/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
+++ CreativeCookies.VideoHosting.API/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
@@ -5,13 +5,16 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using CreativeCookies.VideoHosting.Contracts.Infrastructure.Stripe;
 using CreativeCookies.VideoHosting.Contracts.Services.IdP;
 using CreativeCookies.VideoHosting.Contracts.Services.OAuth;
+using CreativeCookies.VideoHosting.Infrastructure.Azure.Wrappers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Stripe;
 
 namespace CreativeCookies.VideoHosting.API.Areas.Identity.Pages.Account.Manage
 {
@@ -20,17 +23,24 @@ namespace CreativeCookies.VideoHosting.API.Areas.Identity.Pages.Account.Manage
         private readonly IMyHubUserManager _userManager;
         private readonly IMyHubSignInManager _signInManager;
         private readonly IRefreshTokenService _refreshTokenService;
+        private readonly IConnectAccountsService _connectAccountsService;
+        private readonly string _stripeApiSecretKey;
         private readonly ILogger<DeletePersonalDataModel> _logger;
+
 
         public DeletePersonalDataModel(
             IMyHubUserManager userManager,
             IMyHubSignInManager signInManager,
             IRefreshTokenService refreshTokenService,
+            IConnectAccountsService connectAccountsService,
+            StripeSecretKeyWrapper wrapper,
             ILogger<DeletePersonalDataModel> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _refreshTokenService = refreshTokenService;
+            _connectAccountsService = connectAccountsService;
+            _stripeApiSecretKey = wrapper.Value;
             _logger = logger;
         }
 
@@ -98,13 +108,13 @@ namespace CreativeCookies.VideoHosting.API.Areas.Identity.Pages.Account.Manage
                     return Page();
                 }
             }
+            await DeleteStripeEntities(user.StripeCustomerId);
+
             var userId = await _userManager.GetUserIdAsync(user);
             await _refreshTokenService.DeleteIssuedRefreshTokens(Guid.Parse(userId));
 
             Response.Cookies.Delete("stac", cookieOptions);
             Response.Cookies.Delete("ltrt", cookieOptions);
-
-
 
             var result = await _userManager.DeleteAsync(user);
             if (!result.Succeeded)
@@ -117,6 +127,67 @@ namespace CreativeCookies.VideoHosting.API.Areas.Identity.Pages.Account.Manage
             _logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
 
             return Redirect("~/");
+        }
+
+        private async Task DeleteStripeEntities(string stripeCustomerId)
+        {
+            StripeConfiguration.ApiKey = _stripeApiSecretKey;
+
+            var subscriptionService = new SubscriptionService();
+            var subscriptionListOptions = new SubscriptionListOptions
+            {
+                Customer = stripeCustomerId
+            };
+            var requestOptions = await GetRequestOptions();
+            StripeList<Subscription> subscriptions = subscriptionService.List(subscriptionListOptions, requestOptions);
+
+            foreach (var subscription in subscriptions)
+            {
+                string subscriptionId = subscription.Id;
+
+                var paymentService = new PaymentIntentService();
+                var paymentIntents = paymentService.List(
+                    new PaymentIntentListOptions
+                    {
+                        Customer = stripeCustomerId,
+                    }, requestOptions
+                ).ToList();
+
+                if (paymentIntents.Count > 0)
+                {
+                    foreach (var paymentIntent in paymentIntents)
+                    {
+                        var refundService = new RefundService();
+                        var refundOptions = new RefundCreateOptions
+                        {
+                            PaymentIntent = paymentIntent.Id
+                        };
+                        refundService.Create(refundOptions, requestOptions);
+                    }
+                }
+
+                var subscriptionCancelOptions = new SubscriptionCancelOptions()
+                {
+                    Prorate = true
+                };
+
+                subscriptionService.Cancel(subscriptionId, subscriptionCancelOptions, requestOptions);
+
+                // Delete customer
+                var customerService = new CustomerService();
+                customerService.Delete(stripeCustomerId, requestOptions: requestOptions);
+            }
+        }
+
+        private async Task<RequestOptions?> GetRequestOptions()
+        {
+            var accountId = string.Empty;
+
+            accountId = _connectAccountsService.GetConnectedAccountId();
+            var requestOptions = new RequestOptions();
+            requestOptions.StripeAccount = accountId;
+            if (string.IsNullOrWhiteSpace(accountId)) return null;
+            return requestOptions;
         }
     }
 }
