e9ce83d329b878b85ae2e62579a9a2484b55de2f
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-10-11 18:15:09 +0200

    turning HangfireJobId column to nullable

--- CreativeCookies.VideoHosting.DAL/DAOs/OAuth/MyHubUser.cs
+++ CreativeCookies.VideoHosting.DAL/DAOs/OAuth/MyHubUser.cs
@@ -11,6 +11,6 @@ namespace CreativeCookies.VideoHosting.DAL.DAOs.OAuth
         /// <summary>
         /// Id of Hangfire Job, which will create a subscription in the background after 14 days cooling off period
         /// </summary>
-        public string HangfireJobId { get; set; }
+        public string? HangfireJobId { get; set; }
     }
 }
